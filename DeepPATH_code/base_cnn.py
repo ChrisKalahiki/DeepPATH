740bcf522a4f3a403552eba06fd2d473f2aacd05
ChrisKalahiki <chris.kalahiki@gmail.com>
2021-06-18 12:47:46 -0400

    baseCNN.ipynb works

--- DeepPATH_code/base_cnn.py
+++ DeepPATH_code/base_cnn.py
@@ -7,12 +7,40 @@ Just need a quick script to run everything.
 
 import tensorflow as tf
 from tensorflow.keras import layers, models
+import random, os, collections
 # import matplotlib as pyplot
 
 # Import the data
 '''
 I would look at the code from the DeepPATH repo for this. Or my old street sign repo.
 '''
+def make_train_and_test_sets():
+    """Split the data into train and test sets and get the label classes."""
+    train_examples, test_examples = [], []
+    shuffler = random.Random()
+    is_root = True
+    for (dirname, subdirs, filenames) in tf.io.gfile.walk('../../data_cropped/'):
+        # The root directory gives us the classes
+        if is_root:
+            subdirs = sorted(subdirs)
+            classes = collections.OrderedDict(enumerate(subdirs))
+            label_to_class = dict([(x, i) for i, x in enumerate(subdirs)])
+            is_root = False
+        # The sub directories give us the image files for training.
+        else:
+            filenames.sort()
+            shuffler.shuffle(filenames)
+            full_filenames = [os.path.join(dirname, f) for f in filenames]
+            label = dirname.split('/')[-1] # '/' for linux and '\\' for windows
+            label_class = label_to_class[label]
+            examples = list(zip(full_filenames, [label_class] * len(filenames)))
+            num_train = int(len(filenames) * 0.7)
+            train_examples.extend(examples[:num_train])
+            test_examples.extend(examples[num_train:])
+    shuffler.shuffle(train_examples)
+    shuffler.shuffle(test_examples)
+    return train_examples, test_examples, classes
+
 
 # This is the Keras-style CNN in TensorFlow
 model = models.Sequential()
@@ -34,8 +62,8 @@ model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-history = model.fit(train_images, train_labels, epochs=10, 
-                    validation_data=(test_images, test_labels))
+# history = model.fit(train_images, train_labels, epochs=10, 
+#                     validation_data=(test_images, test_labels))
 
 # plt.plot(history.history['accuracy'], label='accuracy')
 # plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
@@ -44,6 +72,6 @@ history = model.fit(train_images, train_labels, epochs=10,
 # plt.ylim([0.5, 1])
 # plt.legend(loc='lower right')
 
-test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+# test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
-print(test_acc)
+# print(test_acc)
